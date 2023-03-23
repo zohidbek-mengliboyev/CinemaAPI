e926203ddeed077c3d8d34cfb2038c859df073a6
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-23 05:27:22 +0500

    Add http get verb

---
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using CinemaAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CinemaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private List<Movie> movies = new List<Movie>
+        {
+            new Movie() { Id = 0, Name = "Mission Impossible 7", Language = "English" },
+            new Movie() { Id = 1, Name = "The Matrix 4", Language = "English" },
+        };
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return movies;
+        }
+    }
+}
---
+++ CinemaAPI/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Language { get; set; }
+    }
+}
