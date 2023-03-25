ffd46c53a4a59e156898f41e51296b9bc1eca2e7
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2023-03-25 11:21:12 +0500

    Some CRUD operations

--- CinemaAPI/Controllers/MoviesController.cs
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -26,27 +26,37 @@ namespace CinemaAPI.Controllers
 
         // GET api/<MoviesController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Movie Get(int id)
         {
-            return "value";
+            var movie = _dbContext.Movies.Find(id);
+            return movie;
         }
 
         // POST api/<MoviesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Movie movie)
         {
+            _dbContext.Movies.Add(movie);
+            _dbContext.SaveChanges();
         }
 
         // PUT api/<MoviesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Movie movieObj)
         {
+            var movie = _dbContext.Movies.Find(id);
+            movie.Name = movieObj.Name;
+            movie.Language = movieObj.Language;
+            _dbContext.SaveChanges();
         }
 
         // DELETE api/<MoviesController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var movie = _dbContext.Movies.Find(id);
+            _dbContext.Movies.Remove(movie);
+            _dbContext.SaveChanges();
         }
     }
 }
