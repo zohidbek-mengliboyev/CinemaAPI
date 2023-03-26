43d0624d666b9a96b1be2911b1dcc5dcb15d4891
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-26 18:15:19 +0500

    Hashing password

---
+++ CinemaAPI/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using AuthenticationPlugin;
+using CinemaAPI.Data;
+using CinemaAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CinemaAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+
+        public UsersController(CinemaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody] User user)
+        {
+            var userWithSameEmail = _dbContext.Users.Where(u => u.Email == user.Email).SingleOrDefault();
+
+            if (userWithSameEmail != null)
+            {
+                return BadRequest("User with same email already exists");
+            }
+            var userObj = new User
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = SecurePasswordHasherHelper.Hash(user.Password),
+                Role = "Users"
+            };
+            _dbContext.Users.Add(userObj);
+            _dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
