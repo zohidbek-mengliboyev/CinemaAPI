ec62f1519593e6e5a403d12e78a02d9c3accd04c
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-26 19:34:53 +0500

    Login Method with unsupported hashtype :(

--- CinemaAPI/Controllers/UsersController.cs
+++ CinemaAPI/Controllers/UsersController.cs
@@ -4,7 +4,9 @@ using CinemaAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 
 namespace CinemaAPI.Controllers
 {
@@ -42,6 +44,36 @@ namespace CinemaAPI.Controllers
             _dbContext.Users.Add(userObj);
             _dbContext.SaveChanges();
             return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] User user)
+        {
+            var userEmail = _dbContext.Users.FirstOrDefault(u => u.Email == user.Email);
+            if (userEmail == null)
+            {
+                return NotFound();
+            }
+            if (!SecurePasswordHasherHelper.Verify(user.Password, userEmail.Password))
+            {
+                return Unauthorized();
+            }
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+            var token = _auth.GenerateAccessToken(claims);
+
+            return new ObjectResult(new
+            {
+                access_token = token.AccessToken,
+                expires_in = token.ExpiresIn,
+                token_type = token.TokenType,
+                creation_Time = token.ValidFrom,
+                expiration_Time = token.ValidTo,
+                user_id = userEmail.Id
+            });
         }
     }
 }
