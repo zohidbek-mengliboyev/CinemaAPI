8961bfa59ef827c09d82c25649b9a537ea8b6de4
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-26 17:45:15 +0500

    Add migration

--- CinemaAPI/Models/Reservation.cs
+++ CinemaAPI/Models/Reservation.cs
@@ -8,7 +8,7 @@ namespace CinemaAPI.Models
         public int Quantity { get; set; }
         public double Price { get; set; }
         public string Phone { get; set; }
-        public DateTime ReservationTime { get; set }
+        public DateTime ReservationTime { get; set; }
 
         public int MovieId { get; set; }
         public int UserId { get; set; }
