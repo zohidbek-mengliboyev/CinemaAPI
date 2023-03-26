9fc332f823b24886f2ff1e264af51a7aa5c90026
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-26 17:02:26 +0500

    Adding model validations

--- CinemaAPI/Models/Movie.cs
+++ CinemaAPI/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CinemaAPI.Models
@@ -6,8 +7,11 @@ namespace CinemaAPI.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name cannot be null or empty")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Language cannot be null or empty")]
         public string Language { get; set; }
+        [Required]
         public double Rating { get; set; }
 
         [NotMapped]
