5759f542bf62bd4872ccf5983ebfcd328c52e69b
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-27 00:17:11 +0500

    Post reservation

---
+++ CinemaAPI/Controllers/ReservationsController.cs
@@ -0,0 +1,31 @@
+﻿using CinemaAPI.Data;
+using CinemaAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CinemaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+
+        public ReservationsController(CinemaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Post(Reservation reservationObj)
+        {
+            reservationObj.ReservationTime = DateTime.Now;
+            _dbContext.Reservations.Add(reservationObj);
+            _dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
