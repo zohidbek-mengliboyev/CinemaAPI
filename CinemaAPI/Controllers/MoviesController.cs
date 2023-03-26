e5980bc22d080f8b4a1b1c1873bc2f81f872accc
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-26 22:50:38 +0500

    Get All Movies

--- CinemaAPI/Controllers/MoviesController.cs
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace CinemaAPI.Controllers
 {
@@ -18,6 +19,22 @@ namespace CinemaAPI.Controllers
             _dbContext = dbContext;
         }
 
+        [HttpGet("[action]")]
+        public IActionResult AllMovies()
+        {
+            var movies = _dbContext.Movies.Select(movie => new
+            {
+                Id = movie.Id,
+                Name = movie.Name,
+                Duration = movie.Duration,
+                Language = movie.Language,
+                Rating = movie.Rating,
+                Genre = movie.Genre,
+                ImageUrl = movie.ImageUrl
+            });
+
+            return Ok(movies);
+        }
 
         [Authorize(Roles = "Admin")]
         [HttpPost]
@@ -36,6 +53,58 @@ namespace CinemaAPI.Controllers
             _dbContext.SaveChanges();
 
             return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromForm] Movie movieObj)
+        {
+            var movie = _dbContext.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound("No record found against this Id");
+            }
+            else
+            {
+                var guid = Guid.NewGuid();
+                var filePath = Path.Combine("wwwroot", guid + ".jpg");
+                if (movieObj.Image != null)
+                {
+                    var fileStream = new FileStream(filePath, FileMode.Create);
+                    movieObj.Image.CopyTo(fileStream);
+                    movie.ImageUrl = filePath.Remove(0, 7);
+                }
+                movie.Name = movieObj.Name;
+                movie.Description = movieObj.Description;
+                movie.Language = movieObj.Language;
+                movie.Duration = movieObj.Duration;
+                movie.PlayingDate = movieObj.PlayingDate;
+                movie.PlayingTime = movieObj.PlayingTime;
+                movie.Rating = movieObj.Rating;
+                movie.Genre = movieObj.Genre;
+                movie.TrailorUrl = movieObj.TrailorUrl;
+                movie.TicketPrice = movieObj.TicketPrice;
+                _dbContext.SaveChanges();
+                return Ok("Record updated successfully");
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var movie = _dbContext.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound("No record found against this Id");
+            }
+            else
+            {
+                _dbContext.Movies.Remove(movie);
+                _dbContext.SaveChanges();
+                return Ok("Record deleted");
+            }
+            
         }
     }
 }
